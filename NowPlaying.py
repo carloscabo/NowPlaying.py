06e578460727611f7011f4b48dab81a563851a13
carloscabo <carlos.cabo@the-cocktail.com>
2016-10-17 14:13:26 +0200

    Initial release

---
+++ NowPlaying.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import json
+
+import urllib
+from urllib.request import urlopen
+from urllib.request import urlretrieve
+
+from TwitterAPI import TwitterAPI
+
+import yaml
+config = yaml.load(open('./config.yml'))
+CONSUMER_KEY        = config['TwitterAPI']['CONSUMER_KEY']
+CONSUMER_SECRET     = config['TwitterAPI']['CONSUMER_SECRET']
+ACCESS_TOKEN_KEY    = config['TwitterAPI']['ACCESS_TOKEN_KEY']
+ACCESS_TOKEN_SECRET = config['TwitterAPI']['ACCESS_TOKEN_SECRET']
+
+def tweetWithImage( filepath, text ):
+    api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+    print('Tw:'+filepath)
+    print('Tw:'+text)
+    file = open( filepath, 'rb')
+    img_data = file.read()
+    r = api.request('statuses/update_with_media', {'status': text }, {'media[]':img_data})
+    print( 'Twitter status:' + str(r.status_code) )
+
+json_fn = "/Users/carloscabo/Library/Application Support/Google Play Music Desktop Player/json_store/playback.json"
+tweet_tpl = "#NowPlaying «{{__ALBUM__}}» by {{__ARTIST__}} "
+
+with open( json_fn ) as data_file:
+    data = json.load(data_file)
+    album = data['song']['album']
+    artist = data['song']['artist']
+    cover_url = data['song']['albumArt']
+
+    urlretrieve( cover_url, './cover.jpg' )
+
+    tweet = tweet_tpl.replace('{{__ARTIST__}}', artist).replace('{{__ALBUM__}}', album)
+    try:
+        tweet += sys.argv[1]
+    except IndexError:
+        print( 'No params' )
+
+    print(tweet)
+    tweetWithImage( './cover.jpg', tweet )
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # NowPlaying.py
-Python 3 script thats tweets #NowPlaying info taken from Google Play Music Desktop Player
+Python 3 script that tweets #NowPlaying info taken from Google Play Music Desktop Player, including album cover
+
+# Requirements
+
+Obviously you need to have installed the <https://www.googleplaymusicdesktopplayer.com/>, and Python 3.
+
+# usage / config
+
+[Create API Keys for your  yout twitter account](https://dev.twitter.com/oauth/overview/application-owner-access-tokens), add then to a file called `config.yml` in the same place the `NowPlaying.py` script is located. You can use the `config.sample.yml` file as template.
+
+To tweet the curren album with its cover execute in your terminal:
+
+```
+python3 NowPlaying.py
+```
+
+If you want to add extra comments / tags to the tweet pass them as string parameter:
+
+```
+python3 NowPlaying.py 'This is the bes #pop album ever :)'
+```
